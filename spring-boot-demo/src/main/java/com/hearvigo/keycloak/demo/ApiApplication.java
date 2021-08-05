8304cb4d42942cf351ee234fca74248c954f58ee
hearvigo <hearvigo@gmail.com>
2021-08-05 09:20:38 +0800

    upgrade keycloak version to 15.0.0

--- spring-boot-demo/src/main/java/com/hearvigo/keycloak/demo/ApiApplication.java
+++ spring-boot-demo/src/main/java/com/hearvigo/keycloak/demo/ApiApplication.java
@@ -1,4 +1,4 @@
-package io.vigoz.keycloak.demo;
+package com.hearvigo.keycloak.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- spring-boot-demo/src/main/java/com/hearvigo/keycloak/demo/controller/HomeController.java
+++ spring-boot-demo/src/main/java/com/hearvigo/keycloak/demo/controller/HomeController.java
@@ -1,6 +1,5 @@
-package io.vigoz.keycloak.demo.controller;
+package com.hearvigo.keycloak.demo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
